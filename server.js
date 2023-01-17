ceb76d03e1c890e3d8e65f939e37d0be56ff9416
cli259Seneca <cli259@myseneca.ca>
2023-01-17 15:15:53 -0500

    update cyclic link

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Cheuk Lung Li  Student ID: 149507212  Date: 17/1/2023
+*  Cyclic Link: https://long-tan-hen-slip.cyclic.app
+*
+********************************************************************************/ 
+
+
 /**************************************************************************** */
 // Setup
 /**************************************************************************** */
